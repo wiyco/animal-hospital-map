3cf0b6c31ea1ef33f96c185a9b7df25280d4f6fe
wiyco <72733890+wiyco@users.noreply.github.com>
2023-06-07 23:47:49 +0900

    Add `Mapbox` layer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 
+import Head from "next/head";
+
 import Header from "@/components/layouts/Header";
 
 export const metadata = {
@@ -10,9 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <body className="bg-zinc-50">
+      <Head>
+        <link href="https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css" rel="stylesheet" />
+      </Head>
+      <body>
         <Header />
-        <main>{children}</main>
+        {children}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import mapboxgl from "mapbox-gl";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <main className=""></main>;
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN ?? "";
+    const map = new mapboxgl.Map({
+      container: "map", // container `id`
+      style: "mapbox://styles/mapbox/streets-v12", // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }, []);
+
+  return (
+    <main>
+      <div id="map" className="w-full h-full overflow-clip"></div>
+    </main>
+  );
 }
