432672932a0c293a5e9112629fceb7f6783c3fa1
KIS000 <88875665+KIS000@users.noreply.github.com>
2023-06-28 09:59:44 +0900

    add something

--- next.config.js
+++ next.config.js
@@ -8,6 +8,28 @@ const nextConfig = {
   //   };
   //   return config;
   // },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/i,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: "1.5em",
+            svgo: false,
+            replaceAttrValues: {
+              "#000000": "{props.color}",
+              black: "{props.color}",
+              "#FFFFFF": "{props.color}",
+              white: "{props.color}",
+            },
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
---
+++ src/components/buttons/MenuButton.tsx
@@ -0,0 +1,7 @@
+import Menu from "@p/icons/menu.svg";
+
+export default function MenuButton() {
+  return (
+    <button><svg><Menu /></svg></button>
+  )
+}
