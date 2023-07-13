52fb05779a3146e5a0dcb318c932fa66bf75be16
wiyco <72733890+wiyco@users.noreply.github.com>
2023-07-14 01:04:51 +0900

    Fixup components related to the `header` (#19)
    - Add `@icons` to `tsconfig` to import icons.
    - Delete `@p` from `tsconfig` .
    - Fixup `MenuButton`.
    - Redesign `Header`.

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -7,24 +7,35 @@ import { MenuButton } from "@/components/buttons/MenuButton";
 
 export default function Header() {
   const [isOpen, setOpen] = useState<boolean>(false);
- 
+
   return (
-  <header>
-    <div className="z-50 absolute p-4 w-full flex items-center justify-between backdrop-blur shadow">
-      <span className="text-lg">
-        <Link href={"."}>あにまっぷ</Link>
-      </span>
-      <MenuButton isOpen={isOpen} setOpen={setOpen}  />
-    </div>
-    <nav className={`z-10 absolute w-full backdrop-blur-sm ${isOpen ? "scale-y-100 top-14" : "scale-y-0"}`}>
-      <ul className="py-4 flex flex-col items-center justify-center space-y-4">
-        <li>トップページ</li>
-        <li>設定</li>
-        <li>アカウント</li>
-        <li>お問い合わせ</li>
-        <li>web予約</li>
-      </ul>
-    </nav>
-  </header>
+    <header>
+      <div className="p-3 w-full flex items-center justify-between backdrop-blur shadow">
+        <span className="text-lg">
+          <Link href={"/"}>あにまっぷ</Link>
+        </span>
+        <MenuButton isOpen={isOpen} setOpen={setOpen} />
+      </div>
+      <nav
+        className={`min-w-[12rem] w-1/2 absolute top-[100%] right-0 backdrop-blur shadow ${
+          isOpen ? "scale-y-100" : "scale-y-0"
+        }`}
+      >
+        <ul className="my-8 flex flex-col items-center justify-center space-y-8">
+          <li>
+            <Link href={"/"}>Web予約</Link>
+          </li>
+          <li>
+            <Link href={"/"}>お問い合わせ</Link>
+          </li>
+          <li>
+            <Link href={"/"}>アカウント</Link>
+          </li>
+          <li>
+            <Link href={"/"}>設定</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
