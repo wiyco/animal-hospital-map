56845f4cb1e58124e0379a3b808bfd4153aa054e
wiyco <72733890+wiyco@users.noreply.github.com>
2023-07-14 01:04:18 +0900

    Fixup components related to the `header`
    - Add `@icons` to `tsconfig` to import icons.
    - Delete `@p` from `tsconfig` .
    - Fixup `MenuButton`.
    - Redesign `Header`.

--- src/components/buttons/MenuButton.tsx
+++ src/components/buttons/MenuButton.tsx
@@ -1,15 +1,23 @@
-import Cancel from "@p/icons/cancel.svg";
-import Menu from "@p/icons/menu.svg";
+import Cancel from "@icons/cancel.svg";
+import Menu from "@icons/menu.svg";
 
-type props = {
+type menuButtonProps = {
   isOpen: boolean;
   setOpen: any;
-}
+};
 
-export function MenuButton({isOpen, setOpen} : props) {
+export function MenuButton({ isOpen, setOpen }: menuButtonProps) {
   return (
     <button onClick={() => setOpen(!isOpen)}>
-      {isOpen ? <span><Cancel /></span> : <span><Menu /></span>}
+      {isOpen ? (
+        <span>
+          <Cancel />
+        </span>
+      ) : (
+        <span>
+          <Menu />
+        </span>
+      )}
     </button>
-    )
+  );
 }
